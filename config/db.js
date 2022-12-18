1c718ada7ae91a8da81e57795c504b52a0453ded
DipSapkota125 <dipsapkota325@gmail.com>
2022-12-18 16:35:02 +0545

    solve error

--- config/db.js
+++ config/db.js
@@ -2,18 +2,13 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
 
 const ConnectDB = async () => {
-  try {
-    await mongoose
-      .connect(process.env.MONGO_URL)
-      .then((data) =>
-        console.log(
-          `MongoDB is connected at:${data.connection.host}`.cyan.underline.bold
-        )
-      );
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+  await mongoose
+    .connect(process.env.MONGO_URL)
+    .then((data) =>
+      console.log(
+        `MongoDB is connected at:${data.connection.host}`.cyan.underline.bold
+      )
+    );
 };
 
 export default ConnectDB;
