dd065e76f1ade8fa112bd6d206d9d361136a6b9e
DipSapkota125 <dipsapkota325@gmail.com>
2022-12-18 16:33:04 +0545

    Add then in db.js

--- config/db.js
+++ config/db.js
@@ -3,10 +3,13 @@ mongoose.set("strictQuery", true);
 
 const ConnectDB = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `MongoDB is connected at : ${connection.host}`.cyan.underline.bold
-    );
+    await mongoose
+      .connect(process.env.MONGO_URL)
+      .then((data) =>
+        console.log(
+          `MongoDB is connected at:${data.connection.host}`.cyan.underline.bold
+        )
+      );
   } catch (error) {
     console.log(error);
     process.exit(1);
